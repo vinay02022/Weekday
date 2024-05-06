8ae82700d90a497830e13d9e7b01949d16e720c7
vinay02022 <vinay01431998@gmail.com>
2024-05-06 16:22:39 +0530

    init

---
+++ weekday-assignment/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   
+          Learn React-init
+     
+    </div>
+  );
+}
+
+export default App;
