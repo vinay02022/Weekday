ce553c0e1577ab7b5906e442c706e5f5da1548f7
vinay02022 <vinay01431998@gmail.com>
2024-05-08 01:42:54 +0530

    Apply Button and referral Button alert Action Done

--- weekday-assignment/src/Components/ApplyButton.js
+++ weekday-assignment/src/Components/ApplyButton.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { Button, Typography } from '@mui/material';
+import AccountCircleSharpIcon from '@mui/icons-material/AccountCircleSharp';
 import BoltTwoToneIcon from '@mui/icons-material/BoltTwoTone';
-const ApplyButton = ({handleClick}) => {
+import Alert from '@mui/material/Alert';
+
+const ApplyButton = () => {
     return (
+        <>
         <Button
             variant="contained"
             color="primary"
@@ -13,18 +17,47 @@ const ApplyButton = ({handleClick}) => {
                 textTransform: 'none', 
                 marginTop:"20px"
             }}
-            onClick={handleClick}
+            onClick={()=>{
+                alert("Hello This is Vinay Pandey and  I am building Backend for Now, Please wait till then 🤪🤪")
+            }}
+           
             aria-label="Apply for this job with one click"
         >
             <BoltTwoToneIcon style={{ fontSize: '2rem', marginRight: '8px', color:'yellow' }} />
             <Typography
                 variant="button"
                 style={{ fontWeight: 'bold', color: 'black' }}
+                
             >
                 Easy Apply
             </Typography>
+            
         </Button>
         
+        <Button
+            variant="contained"
+            color="primary"
+            style={{
+                width: '100%',
+                borderRadius: '10px',
+                textTransform: 'none', 
+                marginTop:"10px"
+            }}
+            onClick={()=>{
+                alert("Hi This is Crafted By Vinay Pandey 🤪🤪")
+            }}
+            aria-label="Apply for this job with one click"
+        >
+             <AccountCircleSharpIcon style={{ fontSize: '2rem', marginRight: '8px' }}/>
+            <Typography
+                variant="contained"
+                style={{ fontWeight: 'bold', color: 'black' }}
+            >
+                Ask For Referral
+            </Typography>
+            
+        </Button>
+        </>
     );
 };
 
